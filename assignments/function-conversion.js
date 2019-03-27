30085453bd4132c39c0abeadb648164a2d8de5c5
arturolei <artie.lei@gmail.com>
2019-03-26 19:47:17 -0700

    Additional modifications made. Starting on stretch goals.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -12,22 +12,21 @@ myFunction();
 // };
 // anotherFunction("Example");
 
-let anotherFunction = (param) => {return param;}
-
+let anotherFunction = param => param;
 console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-let add = (param1,param2) => {return param1+ param2};
+let add = (param1,param2) => param1 + param2;
 console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-let subtract = (param1,param2) => {return param1 - param2};
+let subtract = (param1,param2) =>  param1 - param2;
 console.log(subtract(1,2));
 
 
@@ -39,5 +38,5 @@ console.log(subtract(1,2));
 // });
 // console.log(triple);
 exampleArray = [1,2,3,4];
-const triple = exampleArray.map((num)=>{return num*3});
+const triple = exampleArray.map(num=>num*3);
 console.log(triple);
